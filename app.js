cf1b33185fa068824cbe86670b265ae1518156e7
myriansosa <myriansosa-1972@hotmail.com>
2022-11-30 12:06:30 -0300

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+ 
+const port = 3001;
+
+//Motor de plantillas
+ app.set('view engine', 'ejs');
+//Desarrollo de motores de plantillas
+ app.set('views', __dirname + '/views');
+
+//middleware
+ app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("index", { titulo: "inicio EJS" });
+  });
+
+
+app.get('/Servicios', (req, res) => {
+    res.render('servicios', {tituloServicios : "Este es un mensaje dinámico de servicios"});
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {
+        titulo: "404",
+        descripcion: "Titulo del sitio web"
+    });
+});
+ 
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port);
+});
